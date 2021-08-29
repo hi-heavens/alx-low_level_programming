7587e3dbbb924ae08766b360541f9a389fc82a83
Kehinde Adedokun <ken_adex@yahoo.com>
2021-08-29 06:30:54 -0700

    Confirm a random number is postive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,1 +1,30 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: Get a random number and confirm if the number
+ * is positive, negative, or zero
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%i is negative\n", n);
+	}
+	else
+	{
+		printf("%i is zero\n", n);
+	}
+	return (0);
+}
