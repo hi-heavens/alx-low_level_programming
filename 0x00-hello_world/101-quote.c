e507cde7c95e04f35295febef7c227267603d788
Kehinde Adedokun <ken_adex@yahoo.com>
2021-08-28 22:28:19 -0700

    Printing to the console using the write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,16 @@
 #include <unistd.h>
 
 /**
+ * main - Entry point
  * This program returns the output 1 as correctness
  * The writr function is used in place of printf and puts
  * However, the unistd.h was called for this to be use
  * Our program print out the output
- * End of program
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-	return(1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
