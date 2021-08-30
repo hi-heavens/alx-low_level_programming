69f93fef9d8d0031690ec76591457bb663cbdfcc
Kehinde Adedokun <ken_adex@yahoo.com>
2021-08-30 14:49:55 +0100

    A program that prints the lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A program that prints all single digit 
+ * numbers of base 10 starting from 0
+ * You can only use putchar function and just twice
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
