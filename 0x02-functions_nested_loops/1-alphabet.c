3d2e6aea128c74b2bd428dec64146bdec684bfbd
Kehinde Adedokun <ken_adex@yahoo.com>
2021-08-31 08:04:37 -0700

    updated file to print letter

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,12 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  * Description - A program that that prints the alphabet
  * in lowercase, followed by a new line
  * Return: Always 0 (Success)
  */
-void print_alphabet(void) /** Description - Prototype for print_alphabet */
+void print_alphabet(void)
 {
 	char c = 'a';
 
