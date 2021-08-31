97d10ee154f82744f95c9c9da74d2f6287017072
Kehinde Adedokun <ken_adex@yahoo.com>
2021-08-31 12:32:28 +0100

    A program that print Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description - A program that print the word Holberton
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c[10] = "Holberton";
+	int i;
+
+	for(i = 0; i < 10; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
