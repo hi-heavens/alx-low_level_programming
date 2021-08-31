17fb2c315dfeb7aaea0012e80df89bba266c7d58
Kehinde Adedokun <ken_adex@yahoo.com>
2021-08-31 15:16:11 +0100

    A program that prints the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,10 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - Entry point
+ * Description - A program that that prints the alphabet, 
+ * in lowercase, followed by a new line
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
