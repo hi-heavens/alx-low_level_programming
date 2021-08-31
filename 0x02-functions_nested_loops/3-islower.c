9e27deb26c0a6d9b0f2fda42a1d344cd7d92e678
Kehinde Adedokun <ken_adex@yahoo.com>
2021-08-31 13:47:58 -0700

    A function that checks for lowercase characters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,6 +5,7 @@
 /**
  * _islower - Entry point
  * Description - A function that checks for lowercase character
+ * *@c: the function accepts an input saved into c
  * Return: Always 0 (Success)
  */
 int _islower(int c)
