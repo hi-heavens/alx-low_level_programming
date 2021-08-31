0b99f61eff04ca901461af40255d60bbc4567df5
Kehinde Adedokun <ken_adex@yahoo.com>
2021-08-31 15:27:15 +0100

    A program that prints the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,13 +3,14 @@
 
 /**
  * main - Entry point
- * Description - A program that that prints the alphabet, 
+ * Description - A program that that prints the alphabet,
  * in lowercase, followed by a new line
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
         char c = 'a';
+
         while (c <= 'z')
         {
                 _putchar(c);
