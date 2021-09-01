c9737a04f297c84fb1e5b81f0bb39a23b376825e
Kehinde Adedokun <ken_adex@yahoo.com>
2021-09-01 06:08:48 +0100

    A function that that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,6 +3,7 @@
 /**
  * print_sign - Entry point
  * Description - A function that that prints the sign of a number
+ * @n: the function accepts an input saved into n
  * Returns 1 and prints + if n is greater than zero
  * Returns 0 and prints 0 if n is zero
  * Returns -1 and prints - if n is less than zero
