71aa3c48ab91af743bd1842269d51a5f0661c30d
Kehinde Adedokun <ken_adex@yahoo.com>
2021-09-02 07:10:03 -0700

    that prints the numbers, from 0 to 9, followed by a new line

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,8 @@
 
 /**
  * print_numbers - Entry point
- * Description - that prints the numbers, from 0 to 9, followed by a new line
+ * Description - that prints the numbers, from 0 to
+ * 9, followed by a new line
  * You can only use _putchar twice in your code
  * Return: Always 0 (Success)
  */
