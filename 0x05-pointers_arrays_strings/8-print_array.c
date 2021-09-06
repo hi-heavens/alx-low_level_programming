6b6678279dca51a76cbf2b9725d07b760c6f8420
Kehinde Adedokun <ken_adex@yahoo.com>
2021-09-06 20:22:53 +0100

    A function that prints n elements of an array of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,7 +7,8 @@
  * an array of integers, followed by a new line
  * where n is the number of elements of the array to be printed
  * Numbers must be separated by comma, followed by a space
- * The numbers should be displayed in the same order as they are stored in the array
+ * The numbers should be displayed in the same order
+ * as they are stored in the array
  * You are allowed to use printf
  * *@a: the function accepts an input saved into a
  * *@n: the function accepts an input saved into n
