4b894833c5065465da29facf0cccf6fc43100140
Kehinde Adedokun <ken_adex@yahoo.com>
2021-09-09 18:58:50 +0100

    A function that takes a pointer to an int as parameter and updates the value it points to to 98

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,11 +12,9 @@ char *_strcat(char *dest, char *src)
 {
     int i = 0;
     int j = 0;
-    /*int length = 0;*/
 
     while (dest[i] != '\0')
     {
-        /*length += 1;*/
         i++;
     }
 
