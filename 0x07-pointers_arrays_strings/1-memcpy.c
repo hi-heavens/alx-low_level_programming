94129a64e6126326d31aa463288282abd8f29ac1
Kehinde Adedokun <ken_adex@yahoo.com>
2021-09-13 01:40:03 +0100

    A function that copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,7 +2,6 @@
 /**
 * _memcpy - Entry point
 * Description - A function that copies memory area
-* as parameter and updates the value it points to to 98
 * *@dest: the function accepts an input saved into dest
 * *@src: the function accepts an input saved into src
 * *@n: the function accepts an input saved into n
