1abb22ed7ae96b2244bf3c3188d695d813f75329
Kehinde Adedokun <ken_adex@yahoo.com>
2021-09-15 00:50:32 +0100

    A function that returns the factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* factorial - Entry point
+* Description - A function that returns the factorial
+* of a given number
+* *@n: the function accepts an input saved into n
+* Return: Success (0)
+*/
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+        else if (n < 0)
+        {
+                return (-1);
+        }
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
