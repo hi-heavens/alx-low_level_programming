7226422eb8282ace7abb774de63a1cfecfb0c5c0
Kehinde Adedokun <ken_adex@yahoo.com>
2021-09-15 19:53:37 +0100

    A function that compares 2 strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -35,7 +35,8 @@ if (*s2 == '*')
 }
 return (0);
 }
-
+int bandersnatch(char *s1, char *s2)
+{
 /**
  * bandersnatch - checks recursively for all the paths when the
  * characters are equal
@@ -44,9 +45,6 @@ return (0);
  *
  * Return: return value of wildcmp() or of itself
  */
-
-int bandersnatch(char *s1, char *s2)
-{
 /**
  * if we reached the end of s1, return 0
  * if chars are equal, return the return value of wildcmp()
@@ -59,15 +57,14 @@ if (*s1 == *s2)
 return (bandersnatch(s1 + 1, s2));
 }
 
+char *move(char *s2)
+{
 /**
  * *move - moves the current char past the *
  * @s2: string to iterate over
  *
  * Return: the address of the character after the *
  */
-
-char *move(char *s2)
-{
 /**
  * if the current char is a *
  * increment s2 by 1
