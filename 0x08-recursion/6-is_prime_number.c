eb003c7245fcabfd3a51eda8a466be1073c01f9c
Kehinde Adedokun <ken_adex@yahoo.com>
2021-09-15 02:23:22 +0100

    A function that returns 1 if the input integer is a prime number, otherwise return 0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,21 +7,15 @@
 * *@f: the function accepts a pointer saved into f
 * Return: (0)
 */
-int is_prime_check(int n, int *f)
+int is_not_prime(int n, int i)
 {
-int i;
-
-for (i = 2; i <= n / 2; ++i)
-{
-if (n % i == 0)
-{
-*f = 1;
-break;
-}
-}
+if (i == 1)
+return (1);
+else if (n % i == 0)
 return (0);
+else
+return (is_not_prime(n, i - 1));
 }
-
 
 
 /**
@@ -33,23 +27,9 @@ return (0);
 */
 int is_prime_number(int n)
 {
-int check = 0;
-
-is_prime_check(n, &check);
-
 if (n <= 1)
-{
 return (0);
-}
-else
-{
-if (check == 0)
-{
+else if (is_not_prime(n, n / 2) > 0)
 return (1);
-}
-else
-{
 return (0);
-}
-}
 }
