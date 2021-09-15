2fff76ae662c8ab6d61321d6b531ee7ece6c80cb
Kehinde Adedokun <ken_adex@yahoo.com>
2021-09-15 19:54:14 +0100

    A function that compares 2 strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,68 +11,71 @@ int wildcmp(char *s1, char *s2)
 int sum = 0;
 
 if (*s1 == '\0' && *s2 == '*' && !*move(s2))
-        return (1);
+return (1);
 
 if (*s1 == *s2)
 {
-        if (*s1 == '\0')
-                return (1);
-        return (wildcmp(s1 + 1, s2 + 1));
+if (*s1 == '\0')
+return (1);
+return (wildcmp(s1 + 1, s2 + 1));
 }
 
 if (*s1 == '\0' || *s2 == '\0')
-        return (0);
+return (0);
 
 if (*s2 == '*')
 {
-        s2 = move(s2);
-        if (*s2 == '\0')
-                return (1);
-        if (*s1 == *s2)
-                sum += wildcmp(s1 + 1, s2 + 1);
-        sum += bandersnatch(s1 + 1, s2);
-        return (!!sum);
+s2 = move(s2);
+if (*s2 == '\0')
+return (1);
+if (*s1 == *s2)
+sum += wildcmp(s1 + 1, s2 + 1);
+sum += bandersnatch(s1 + 1, s2);
+return (!!sum);
 }
 return (0);
 }
+
+/**
+* bandersnatch - checks recursively for all the paths when the
+* characters are equal
+* @s1: first string
+* @s2: second string
+*
+* Return: return value of wildcmp() or of itself
+*/
+
 int bandersnatch(char *s1, char *s2)
 {
 /**
- * bandersnatch - checks recursively for all the paths when the
- * characters are equal
- * @s1: first string
- * @s2: second string
- *
- * Return: return value of wildcmp() or of itself
- */
-/**
- * if we reached the end of s1, return 0
- * if chars are equal, return the return value of wildcmp()
- * increment s1 by 1, not s2
- */
+* if we reached the end of s1, return 0
+* if chars are equal, return the return value of wildcmp()
+* increment s1 by 1, not s2
+*/
 if (*s1 == '\0')
-        return (0);
+return (0);
 if (*s1 == *s2)
-        return (wildcmp(s1, s2));
+return (wildcmp(s1, s2));
 return (bandersnatch(s1 + 1, s2));
 }
+
+/**
+* *move - moves the current char past the *
+* @s2: string to iterate over
+*
+* Return: the address of the character after the *
+*/
 
 char *move(char *s2)
 {
 /**
- * *move - moves the current char past the *
- * @s2: string to iterate over
- *
- * Return: the address of the character after the *
- */
-/**
- * if the current char is a *
- * increment s2 by 1
- * else return the address of
- * the first char past all *
- */
+* if the current char is a *
+* increment s2 by 1
+* else return the address of
+* the first char past all *
+*/
 if (*s2 == '*')
-        return (move(s2 + 1));
+return (move(s2 + 1));
 else
-        return (s2);
+return (s2);
 }
