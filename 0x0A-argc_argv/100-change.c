72ee1eefe31d633d2af3b598a5c4044ccdcd7940
Kehinde Adedokun <ken_adex@yahoo.com>
2021-09-16 23:58:53 -0700

    A program that prints the minimum number of coins to make change for an amount of money

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,39 +10,38 @@
 */
 int returnCoin (int cents)
 {
-int coins = 0;
+	int coins = 0;
 
-while (cents > 0)
-{
-while (cents >= 25)
-{
-cents -= 25;
-coins++;
-}
-while (cents >= 10)
-{
-cents -= 10;
-coins++;
-}
-while (cents >= 5)
-{
-cents -= 5;
-coins++;
-}
-while (cents >= 2)
-{
-cents -= 2;
-coins++;
-}
-while (cents >= 1)
-{
-cents -= 1;
-coins++;
-}
-return (coins);
-}
-
-return (0);
+	while (cents > 0)
+	{
+		while (cents >= 25)
+		{
+			cents -= 25;
+			coins++;
+		}
+		while (cents >= 10)
+		{
+			cents -= 10;
+			coins++;
+		}
+		while (cents >= 5)
+		{
+			cents -= 5;
+			coins++;
+		}
+		while (cents >= 2)
+		{
+			cents -= 2;
+			coins++;
+		}
+		while (cents >= 1)
+		{
+			cents -= 1;
+			coins++;
+		}
+		return (coins);
+	}
+	return (0);
 }
 
 
