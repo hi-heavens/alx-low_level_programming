15eb06ac40251123fb9158eeef45f959abb6545a
Kehinde Adedokun <ken_adex@yahoo.com>
2021-09-17 03:03:25 +0100

    A program that prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description - A program that prints all arguments it receives
+* *@argc: the function accepts an input saved into argc
+* *@argv: the function accepts an input saved into argv
+* Return: Success (0)
+*/
+int main(int argc, char *argv[])
+{
+        int argcount = 0;
+
+        while (argcount < argc)
+        {
+                printf("%s\n", argv[argcount]);
+                argcount++;
+        }
+
+        return (0);
+}
