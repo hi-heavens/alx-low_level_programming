b24a2e21e5ca501b61e1c8317c582d5768112d28
Kehinde Adedokun <ken_adex@yahoo.com>
2021-09-21 07:57:24 -0700

    A function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,30 +9,47 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-char *a;
-int i, j, c, d;
+	char *a;
+	int i, j, c, d;
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
 
-for (i = 0; s1[i] != '\0'; i++)
-;
-for (j = 0; s2[j] != '\0'; j++)
-;
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 
-a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
-if (a == NULL)
-return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		;
+	}
 
-for (c = 0, d = 0; c < (i + j + 1); c++)
-{
-if (c < i)
-a[c] = s1[c];
-else
-a[c] = s2[d++];
-}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		;
+	}
 
-return (a);
+	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
+
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+
+	for (c = 0, d = 0; c < (i + j + 1); c++)
+	{
+		if (c < i)
+		{
+			a[c] = s1[c];
+		}
+		else
+		{
+			a[c] = s2[d++];
+		}
+	}
+
+	return (a);
 }
