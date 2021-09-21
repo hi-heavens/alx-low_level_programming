b88b5614ea7d2c927f948252304cada2625314dc
Kehinde Adedokun <ken_adex@yahoo.com>
2021-09-21 08:24:53 -0700

    A function that concatenates all the arguments of your program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,36 +11,32 @@
 */
 char *argstostr(int ac, char **av)
 {
-{
-char *a, *retp;
-int i, j, total;
+	char *a, *retp;
+	int i, j, total;
 
-if (ac == 0 || av == NULL)
-return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0, total = 0; i < ac; i++)
+	{
+		for (j = 0; *(*(av + i) + j) != '\0'; j++, total++)
+			;
+		total++;
+	}
+	total++;
+	a = malloc(total * sizeof(char));
+	if (a == NULL)
+		return (NULL);
 
-for (i = 0, total = 0; i < ac; i++)
-{
-for (j = 0; *(*(av + i) + j) != '\0'; j++, total++)
-;
-total++;
-}
-total++;
-
-a = malloc(total * sizeof(char));
-if (a == NULL)
-return (NULL);
-
-retp = a;
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j] != '\0'; j++)
-{
-*a = av[i][j];
-a++;
-}
-*a = '\n';
-a++;
-}
-
-return (retp);
+	retp = a;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			*a = av[i][j];
+			a++;
+		}
+		*a = '\n';
+		a++;
+	}
+	return (retp);
 }
