677b92be40f5e61600b452d8361d9e549ba8f994
Kehinde Adedokun <ken_adex@yahoo.com>
2021-09-28 15:25:41 -0700

    Defining new construct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog- main entry
+ * Description - Creation of a new data type called dog
+ * *@name: This takes an input as name
+ * *@age: This takes an input as age
+ * *@owner: This takes an input as the owner
+ */
 struct dog
 {
 	char *name;
