ea21d8a2de81010e1d6591f349fa24d73d4b4771
Kehinde Adedokun <ken_adex@yahoo.com>
2021-10-18 13:27:01 -0700

    A function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,38 +3,36 @@
 * create_file - Entry point
 * Description - A function that creates a file
 * *@filename: the function accepts an input saved into filename
-* *@letters: letters is the number of letters it should read and print
+* *@text_content: letters is the number of letters it should read and print
 * Return: Success (1)
 */
-int toReturn(int toCheck);
+
 int create_file(const char *filename, char *text_content)
 {
-int toOpen, toWrite;
-int contentLen = 0;
+	int toOpen, toWrite;
+	int contentLen = 0;
 
-if (filename == NULL)
-return (-1);
+	if (filename == NULL)
+		return (-1);
 
-while(text_content[contentLen] != '\0')
-contentLen++;
+	while (text_content[contentLen] != '\0')
+		contentLen++;
 
-/* Opening of the file */
-toOpen = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-/* To check if there is an error of some sort */
-toReturn(toOpen);
+	/* Opening of the file */
+	toOpen = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-/* Writing the already read file */
-toWrite = write(STDOUT_FILENO, text_content, contentLen);
-/* To check if there is an error of some sort */
-toReturn(toWrite);
+	/* To check if there is an error of some sort */
+	if (toOpen == -1)
+		return (-1);
 
-close(toOpen);
+	/* Writing the already read file */
+	toWrite = write(STDOUT_FILENO, text_content, contentLen);
 
-return (1);
-}
+	/* To check if there is an error of some sort */
+	if (toWrite == -1)
+		return (-1);
 
-int toReturn(int toCheck)
-{
-if (toCheck == -1 )
-return (-1);
+	close(toOpen);
+
+	return (1);
 }
