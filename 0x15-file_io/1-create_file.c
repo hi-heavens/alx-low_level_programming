ff5c6fa5b34e6bebcbb7f00c4cac37e24472d057
Kehinde Adedokun <ken_adex@yahoo.com>
2021-10-18 21:00:22 +0100

    A function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,32 +9,32 @@
 int toReturn(int toCheck);
 int create_file(const char *filename, char *text_content)
 {
-        int toOpen, toWrite;
-        int contentLen = 0;
+int toOpen, toWrite;
+int contentLen = 0;
 
-        if (filename == NULL)
-                return (-1);
+if (filename == NULL)
+return (-1);
 
-        while(text_content[contentLen] != '\0')
-            contentLen++;
+while(text_content[contentLen] != '\0')
+contentLen++;
 
-        /* Opening of the file */
-        toOpen = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-        /* To check if there is an error of some sort */
-        toReturn(toOpen);
+/* Opening of the file */
+toOpen = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+/* To check if there is an error of some sort */
+toReturn(toOpen);
 
-        /* Writing the already read file */
-        toWrite = write(STDOUT_FILENO, text_content, contentLen);
-        /* To check if there is an error of some sort */
-        toReturn(toWrite);
+/* Writing the already read file */
+toWrite = write(STDOUT_FILENO, text_content, contentLen);
+/* To check if there is an error of some sort */
+toReturn(toWrite);
 
-        close(toOpen);
+close(toOpen);
 
-        return (1);
+return (1);
 }
 
 int toReturn(int toCheck)
 {
-    if (toCheck == -1 )
-            return (-1);
+if (toCheck == -1 )
+return (-1);
 }
