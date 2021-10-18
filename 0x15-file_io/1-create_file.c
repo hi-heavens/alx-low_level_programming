80417c5375c9606f9d99263bcc7054b7e82c8271
Kehinde Adedokun <ken_adex@yahoo.com>
2021-10-18 20:58:40 +0100

    A function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+* create_file - Entry point
+* Description - A function that creates a file
+* *@filename: the function accepts an input saved into filename
+* *@letters: letters is the number of letters it should read and print
+* Return: Success (1)
+*/
+int toReturn(int toCheck);
+int create_file(const char *filename, char *text_content)
+{
+        int toOpen, toWrite;
+        int contentLen = 0;
+
+        if (filename == NULL)
+                return (-1);
+
+        while(text_content[contentLen] != '\0')
+            contentLen++;
+
+        /* Opening of the file */
+        toOpen = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+        /* To check if there is an error of some sort */
+        toReturn(toOpen);
+
+        /* Writing the already read file */
+        toWrite = write(STDOUT_FILENO, text_content, contentLen);
+        /* To check if there is an error of some sort */
+        toReturn(toWrite);
+
+        close(toOpen);
+
+        return (1);
+}
+
+int toReturn(int toCheck)
+{
+    if (toCheck == -1 )
+            return (-1);
+}
