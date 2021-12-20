fee4ddb051394d807ca0c711e854c3eed8b296d8
Kehinde Adedokun <ken_adex@yahoo.com>
2021-12-20 18:32:16 +0100

    A function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,36 +10,36 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *p_dott;
-    int i, j;
+dog_t *p_dott;
+int i, j;
 
-    p_dott = malloc(sizeof(*p_dott));
+p_dott = malloc(sizeof(*p_dott));
 
-    if (p_dott == NULL)
-    {
-        free(p_dott);
-        return NULL;
-    }
-    for (i = 0; name[i]; i++)
-        ;
-    p_dott->name = malloc(i++);
-    for (j = 0; j < i; j++)
-    {
-        p_dott->name[j] = name[j];
-    }
-    p_dott->name[j] = '\0';
+if (p_dott == NULL)
+{
+free(p_dott);
+return NULL;
+}
+for (i = 0; name[i]; i++)
+;
+p_dott->name = malloc(i++);
+for (j = 0; j < i; j++)
+{
+p_dott->name[j] = name[j];
+}
+p_dott->name[j] = '\0';
 
-    p_dott->age = age;
+p_dott->age = age;
 
-    for (i = 0; owner[i]; i++)
-        ;
-    p_dott->owner = malloc(i++);
-    for (j = 0; j < i; j++)
-    {
-        p_dott->owner[j] = owner[j];
-    }
-    p_dott->owner[j] = '\0';
+for (i = 0; owner[i]; i++)
+;
+p_dott->owner = malloc(i++);
+for (j = 0; j < i; j++)
+{
+p_dott->owner[j] = owner[j];
+}
+p_dott->owner[j] = '\0';
 
 
-    return (p_dott);
+return (p_dott);
 }
