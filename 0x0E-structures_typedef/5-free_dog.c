bf6127f5e80a407e0347b65b2d969b695e808ac0
Kehinde Adedokun <ken_adex@yahoo.com>
2022-04-12 01:51:25 +0100

    Edit the file

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -6,25 +6,6 @@
 * *@d: the function accepts a pointer input saved into d
 * Return: Nothing for now
 */
-typedef struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
-
-void free_dog(dog_t *d);
-
-int main(void)
-{
-    dog_t *my_dog;
-
-    my_dog = new_dog("Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
-    free_dog(my_dog);
-    return (0);
-}
-
 void free_dog(dog_t *d)
 {
 if (d)
