4f559b6b054e28b348e6050a92b9ac46b3f17edb
Kehinde Adedokun <ken_adex@yahoo.com>
2022-04-12 01:49:40 +0100

    An updated dog file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,25 +1,33 @@
 #ifndef DOG_H
 #define DOG_H
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * struct dog- main entry
- * Description - Creation of a new data type called dog
- * *@name: This takes an input as name
- * *@age: This takes an input as age
- * *@owner: This takes an input as the owner
+ * struct dog - A dog struct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Define a new type struct dog
  */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
+
 };
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
 void free_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
+
 
 
 #endif
