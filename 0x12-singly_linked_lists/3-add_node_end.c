5f07585b280e6f5bbc5f6db4f9c0dd0389060332
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2022-04-22 06:01:08 +0100

    Updated result for task 3 and 4

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,50 +1,34 @@
 #include "lists.h"
-#include <string.h>
-#include <stdio.h>
 
 /**
- * add_node_end - add a new node at the end of `list_t` list
- * @head: double pointer to head
- * @str: string to be saved in new node, must be duplicated
- * Return: Address of new element or NULL if failed
+ * add_node_end - Function entry
+ * Description: A function that adds a new node at the end of a list_t list
+ * @head: A pointer to the address of the head element
+ * @str: String to duplicate
+ * Return: address of the new element
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *temp;
-	char *dup_str;
-	int len;
+	list_t *newnode, *temp;
 
-	for (len = 0; str[len] != '\0'; len++)
-		;
-	new = malloc(sizeof(list_t));
+	newnode = (list_t *)malloc(sizeof(list_t));
 
-	if (new == NULL)
-	{
-		free(new);
+	if (newnode == NULL)
 		return (NULL);
-	}
 
-	dup_str = strdup(str);
+	newnode->str = strdup(str);
+	newnode->len = strlen(str);
+	newnode->next = NULL;
 
-	new->str = dup_str;
+	temp = *head;
 
-	if (dup_str == NULL)
-	{
-		free(NULL);
-		return (NULL);
-	}
-
-	new->len = len;
-	new->next = NULL;
-
-	if (*head == NULL)
-		*head = new;
+	if (temp == NULL)
+		*head = temp = newnode;
 	else
 	{
-		temp = *head;
 		while (temp->next != NULL)
 			temp = temp->next;
-		temp->next = new;
+		temp->next = newnode;
 	}
 
 	return (*head);
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,19 +1,20 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * free_list - Frees a list_t list.
- * @head: A pointer to the list_t list.
+ * free_list - Function entry
+ * Description: A function that frees a list_t list
+ * @head: A pointer to the address of the head element
+ * Return: See nothing!
  */
 void free_list(list_t *head)
 {
-	list_t *tmp;
+	list_t *temp;
 
-	while (head)
+	while (head != NULL)
 	{
-		tmp = head->next;
+		temp = head->next;
 		free(head->str);
 		free(head);
-		head = tmp;
+		head = temp;
 	}
 }
