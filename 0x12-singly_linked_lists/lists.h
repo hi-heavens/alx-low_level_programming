4b9950339424b48fddb11b2ba1044516c1fd09d8
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2022-04-22 05:18:51 +0100

    Updated header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,15 +1,19 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LIST_H
+#define LIST_H
 
+#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
+int _putchar(char c);
+
 /**
- * struct list_s - main entry
- * Description - A singly linked list of node structure
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
- * Return: Success (0)
+ *
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
@@ -17,10 +21,11 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
-
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
+
 #endif
